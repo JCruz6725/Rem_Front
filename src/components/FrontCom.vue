86e99f7d70b90ca1c2c6ada7a7e99ad0b3545a1e
JohnCruz <john.cruz6725@gmail.com>
2022-05-18 14:37:46 -0500

    moving and testing things on the page. adding dividers

--- src/components/FrontCom.vue
+++ src/components/FrontCom.vue
@@ -5,46 +5,34 @@
         <div class="container-fluid">
           <h1 class="display-5 fw-bold">{{ person.full_name }} | {{resume.title}} | {{person.location}}</h1>
           <p>{{ resume.summary }}</p>
-          <div class="container bg-danger">
+          <div class="container rounded">
             <div class="row">
-              <div class="col border rounded-10"> {{resume.skills}} </div>
+              <div class="col border"> {{resume.skills}} </div>
               <div class="col border"> {{resume.related_courses}} </div>
-              <div class="col border">  </div>
-              
-              
+              <div class="col border"> asdfasdf </div>
             </div>
           </div>
-
-
           <button class="btn btn-primary btn-lg" type="button"> programmeing skills????? </button>
         </div>
       </div>
     </div>
-
-
-
-
-
-
-    <div class="container">
-      <div v-for="(p, i) in professionals" :key="i">
-        <div class="p-3 mb-4 bg-light border rounded">
-          <div class="container-fluid py-5"> 
-          <h4 class="display-10 fw-bold"> {{ p.employer_name }} | {{ p.position }} | {{p.title_of_project}} | {{ p.time_at }} </h4>
-          <p> {{ p.summary }} </p>
-          <button class="btn btn-primary btn-lg" type="button">eCommerce store</button>
+    <div class="container ">
+      <p class="fw-bold text-start border-bottom">Professional Work History</p>
+      <div class="container-lg">
+        <div v-for="(p, i) in professionals" :key="i">
+          <div class="p-3 mb-4 bg-light border rounded">
+            <div class="container-fluid py-5"> 
+            <h4 class="display-10 fw-bold"> {{ p.employer_name }} | {{ p.position }} | {{p.title_of_project}} | {{ p.time_at }} </h4>
+            <p> {{ p.summary }} </p>
+            <button class="btn btn-primary btn-lg" type="button">eCommerce store</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-
-
-
-
-
     <div v-for="(p, i) in projects" :key="i" class="container-fluid bg-secondary">
-      <div class="row justify-content-center ">
+      <div class="row justify-content-center p-1">
           <div class="col-md-6 t-5 b-5">
             <div class=" p-5 text-dark bg-light border rounded-3">
               <h4>{{p.project_name}} | {{p.tech_used}} </h4>
@@ -54,8 +42,6 @@
           </div>
       </div>
     </div>
-
-
   </div>
 </template>
 
@@ -64,9 +50,6 @@
 <script>
 
 import axios from 'axios'
-//import { response } from 'express'
-
-
 
 export default {
   name: 'FrontCom',
@@ -92,12 +75,6 @@ export default {
         this.education = response.data.education
         this.projects = response.data.project
         this.professionals = response.data.professional
-        
-        
-
-
-
-        
         //console.log(response)
       })
   },
