542557bf2f07801b4f677df59fdad177a0977d0f
JohnCruz <john.cruz6725@gmail.com>
2022-05-18 23:50:15 -0500

    design

--- src/components/FrontCom.vue
+++ src/components/FrontCom.vue
@@ -1,51 +1,62 @@
 <template>
   <div class="FrontCom">
-    <div class="container py-4">
-      <div class="p-5 mb-4 bg-light border rounded-3">
+    
+      <div class="container p-5 mb-4 bg-light border rounded-3">
         <div class="container-fluid">
           <h2 class="fw-bold">{{ person.full_name }} | {{resume.title}} | {{person.location}}</h2>
-          <p class="container border rounded">{{ resume.summary }}</p>
-            <div class="container border rounded">
-              <div class="row">programming lang:
-                <div class="col border">{{resume.programming_skills}}</div>
-              </div>
-              <div class="row"> related_courses:
-                <div class="col border"> {{resume.related_courses}} </div>
-              </div>
-              <div class="row"> misc_tools:
-                <div class="col border"> {{resume.office_tools}} </div>
-              </div>
-                <div class="row"> programming tools: 
-                  <div class="col border"> {{resume.industry_tools}} </div>
-                </div>
-              </div>
-            </div>
-        </div>
-      </div>
+          <p class="container rounded">{{ resume.summary }}</p>
 
-
-
-    <div class="container ">
-      <p class="fw-bold text-start border-bottom">Eduaction Background</p>
-      <div class="container-lg">
-        <div class="p-5 mb-4 bg-light border rounded-3">
-          <div v-for ="(e, i) in education" :key="i" class="container-fluid border">
+    <div class="container py-3">
+      <p class="fw-bold text-start border-bottom  ">Education Background</p>
+      <div class="container-lg  rounded-3">
+        
+          <div v-for ="(e, i) in education" :key="i" class="container-fluid">
             <h4> {{e.institution_name}} | {{e.location}} | {{e.time_at}} </h4>
             <h5> {{e.degree}} </h5>
             </div>
-        </div>
+        
       </div>
     </div>
+
+
+<div class="container">  
+  <p class="fw-bold text-start border-bottom">Skills</p>
+
+            <div class="container py-3 bg rounded">
+              <div class="row fw-bold">Known Programming Languages:
+                <div class="col fw-normal">{{resume.programming_skills}}</div>
+              </div>
+              <div class="row fw-bold"> Courses Taken:
+                <div class="col fw-normal"> {{resume.related_courses}} </div>
+              </div>
+              <div class="row fw-bold"> Office Tools:
+                <div class="col fw-normal"> {{resume.office_tools}} </div>
+              </div>
+                <div class="row fw-bold"> Tech tools: 
+                  <div class="col fw-normal"> {{resume.industry_tools}} </div>
+                </div>
+              </div>
+
+</div>
+
+
+            </div>
+        </div>
+      
+
+
+
+
 
 
 
 
    
-    <div class="container ">
+    <div class="container py-4">
       <p class="fw-bold text-start border-bottom">Professional Work History</p>
       <div class="container-lg">
         <div v-for="(p, i) in professionals" :key="i">
-          <div class="p-3 mb-4 bg-light border rounded">
+          <div class="p-5 mb-4 bg-light border rounded-3">
             <div class="container-fluid py-5"> 
             <h4 class="display-10 fw-bold"> {{ p.employer_name }} | {{ p.position }} | {{p.title_of_project}} | {{ p.time_at }} </h4>
             <p> {{ p.summary }} </p>
@@ -56,7 +67,7 @@
       </div>
     </div>
 
-    <div v-for="(p, i) in projects" :key="i" class="container-fluid bg-secondary">
+    <div v-for="(p, i) in projects" :key="i" class="container-fluid bg-secondary bg-opacity-50">
       <div class="row justify-content-center p-1">
           <div class="col-md-6 t-5 b-5">
             <div class=" p-5 text-dark bg-light border rounded-3">
--- src/components/HeaderCom.vue
+++ src/components/HeaderCom.vue
@@ -1,33 +1,30 @@
 <template>
+
+<!-- add an if statement to wait while the stuff loads in. -->
+
+
+
   <div class="HeaderCom">
-    <div class="container bg-danger">
+    <div class="container">
       <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
           <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
         </a>
-
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
           <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-          <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
-          <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
-          <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
-          <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+          <li><a href="https://github.com/JCruz6725" target="_blank" class="nav-link px-2 link-dark">GitHub</a></li>
+          <li><a href="https://www.hackerrank.com/john_cruz6725" target="_blank" class="nav-link px-2 link-dark">HackerRank</a></li>
         </ul>
-
         <div class="col-md-3 text-end">
           <button type="button" class="btn btn-outline-primary me-2">Login</button>
-          
         </div>
       </header>
     </div>
   </div>
 </template>
 
-
-
 <script>
 export default {
   name: 'HeaderCom',
-
 }
 </script>
