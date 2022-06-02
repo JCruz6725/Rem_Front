c8effaf3e5c083a8dbfec04242fad36c5d37dd74
JohnCruz <john.cruz6725@gmail.com>
2022-06-02 12:08:13 -0500

    added login dropdown with text fields

--- src/components/HeaderCom.vue
+++ src/components/HeaderCom.vue
@@ -17,7 +17,30 @@
           <li><a href="#contact_me_info" target="" class="nav-link px-2 link-dark">Contact Me</a></li> <!-- need to make this move to contact me part of page -->
         </ul>
         <div class="col-md-3 text-end">
-          <button type="button" class="btn btn-outline-primary me-2">Login</button>
+          <div class="dropdown">
+            <button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+              Login
+            </button>
+            <form class="dropdown-menu p-4">
+              <div class="mb-3">
+                <label for="exampleDropdownFormEmail2" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
+              </div>
+              <div class="mb-3">
+                <label for="exampleDropdownFormPassword2" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
+              </div>
+              <div class="mb-3">
+                <div class="form-check">
+                  <input type="checkbox" class="form-check-input" id="dropdownCheck2">
+                  <label class="form-check-label" for="dropdownCheck2">
+                    Remember me
+                  </label>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
+          </div>
         </div>
       </header>
     </div>
